9c3580ee086ea553f8e521dc75d3436596689316
oneclick insurer <oneclickinsurer@gmail.com>
2025-10-06 11:39:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4148377187-d352f",
+  "appId": "1:1005920234739:web:8bb2a5c296bf8c28481b86",
+  "apiKey": "AIzaSyBeUwwF7Su5uxOJ4YNWEUgvQuIUgod3RZc",
+  "authDomain": "studio-4148377187-d352f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1005920234739"
+};
