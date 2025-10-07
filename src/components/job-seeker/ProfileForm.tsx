cf816bd1418221fabebf9f39d2ce866a897b4936
oneclick insurer <oneclickinsurer@gmail.com>
2025-10-07 11:48:00 +0000

    Runtime Error
    Error: React.Children.only expected to receive a single

--- src/components/job-seeker/ProfileForm.tsx
+++ src/components/job-seeker/ProfileForm.tsx
@@ -128,17 +128,17 @@ export function ProfileForm() {
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Experience Level</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
+                <FormControl>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
                     <SelectTrigger>
                       <SelectValue placeholder="Select your experience level" />
                     </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="fresher">Fresher</SelectItem>
-                    <SelectItem value="experienced">Experienced</SelectItem>
-                  </SelectContent>
-                </Select>
+                    <SelectContent>
+                      <SelectItem value="fresher">Fresher</SelectItem>
+                      <SelectItem value="experienced">Experienced</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
