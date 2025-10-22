299aedf3e26d0ba02e722061f857f68cd8c94809
oneclick insurer <oneclickinsurer@gmail.com>
2025-10-22 06:04:07 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -36,9 +36,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
-  },
+  serverExternalPackages: ['firebase-admin'],
 };
 
 export default nextConfig;
