663a3872f58f8591b81ebaaefc70fe0d7fefbbd2
oneclick insurer <oneclickinsurer@gmail.com>
2025-10-22 06:02:56 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'oneclickinsurer.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
